8ef6ea6f13e502aca0422aa6e3cfe38b2a40e3c3
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-08-24 02:04:34 -0400

    add footer

--- src/views/Landing/index.jsx
+++ src/views/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../../components/Footer'
 import Navbar from '../../components/Navbar'
 import AboutProduct from './AboutProduct'
 import DetailsProducts from './DetailsProducts'
@@ -13,6 +14,7 @@ const Landing = () => {
             <Feactures />
             <Store />
             <AboutProduct />
+            <Footer />
         </>
     )
 }
